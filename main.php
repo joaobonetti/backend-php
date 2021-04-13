abb629b1268ba2ea1c99e72820507dbf4912a33a
João Bonetti <joaopaulodalmeida2@gmail.com>
2021-04-12 23:45:45 -0300

    function transfer

--- main.php
+++ main.php
@@ -3,11 +3,15 @@
 // require_once __DIR__ . '/vendor/autoload.php';
 
 require_once ('src/Session.php');
+require_once ('src/Operations.php');
 
 // use Moovin\Job\Backend;
 // use Config\Config;
 // use Src\Session;
 
 $session = new Session;
-$details = $session->startSession();
+$count = $session->startSession();
+
+$operations = new Operations;
+$details['balance'] = $operations->deposit($details['balance'], 500.78);
 print_r($details);
--- src/Operations.php
+++ src/Operations.php
@@ -2,7 +2,18 @@
 
 class Operations
 {
-    public function deposit(){
-        
+    public function deposit($balance, $valueDeposit){
+        return $balance + $valueDeposit;
     }
+    
+    public function withdraw($balance, $valueWithdraw){
+        return $balance - $valueWithdraw;
+    }
+
+    public function transfer($from, $to, $value) {
+        
+
+    }
+
+
 }
