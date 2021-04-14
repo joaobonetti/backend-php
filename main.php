45fa83fc4cb5c8b6058f9d65016ef8f8f8798cb9
João Bonetti <joaopaulodalmeida2@gmail.com>
2021-04-13 23:24:23 -0300

    adc number_format

--- main.php
+++ main.php
@@ -18,13 +18,13 @@ foreach ($details as $key => $value) {
     $$key = $value;
 }
 
-echo "Sessão iniciada com conta de número " . $count . " do tipo " . $type . " com B$" . $balance . " de saldo \n \n";
+echo "\nSessão iniciada com conta de número " . $count . " do tipo " . $type . " com B$" . number_format($balance, 2, ',', '.') . " de saldo \n \n";
 
 $operations = new Operations;
 $balance = $operations->deposit($balance, 500.78);
 
 $balance = $operations->withdraw($balance, 780.00, $type);
 
-$operations->transfer($count, $balance, array_rand(BANK_ACCOUNT), 1);
+$operations->transfer($count, $balance, array_rand(BANK_ACCOUNT), 950.45);
 
-echo " Sessão finalizada";
+echo "Sessão finalizada\n\n";
--- src/Operations.php
+++ src/Operations.php
@@ -4,7 +4,7 @@ class Operations
 {
     public function deposit($balance, $valueDeposit){
         $balance += $valueDeposit;
-        echo "Foram depositados B$" . $valueDeposit . " na conta. Saldo atual é de " . $balance . "\n \n";
+        echo "Foram depositados B$" . number_format($valueDeposit, 2, ',', '.') . " na conta. Saldo atual é de B$" . number_format($balance, 2, ',', '.') . "\n \n";
         return $balance;
     }
     
@@ -13,7 +13,7 @@ class Operations
         $limit = ($typeAccount == 'CC') ? 600 : 1000;
         if($balance >= ($valueWithdraw + $discount) && $valueWithdraw <= $limit) {
             $balance -= $valueWithdraw - $discount;
-            echo "Foram sacados B$" . $valueWithdraw . " da conta. A taxa de operação é de: B$" . $discount . ". Saldo atual é de " . $balance . "\n \n";
+            echo "Foram sacados B$" .  number_format($valueWithdraw, 2, ',', '.') . " da conta. A taxa de operação é de: B$" . number_format($discount, 2, ',', '.') . ". Saldo atual é de B$" . number_format($balance, 2, ',', '.') . "\n \n";
             return $balance;
         }
         else {
@@ -27,13 +27,13 @@ class Operations
             echo "Não é possível transferir para a própria conta \n \n";
             return False;
         } else if($balanceFrom <= $value) {
-            echo "A conta não possui saldo suficiente para a transferência. Saldo em conta: B$" . $balanceFrom . ". Valor da trasnferência: B$" . $value . "\n \n";
+            echo "A conta não possui saldo suficiente para a transferência. Saldo em conta: B$" . number_format($balanceFrom, 2, ',', '.') . ". Valor da transferência: B$" . number_format($value, 2, ',', '.') . "\n \n";
             return False;
         } else {
             $balanceFrom -= $value;
             $balanceTo = BANK_ACCOUNT[$to]['balance'];
             $balanceTo += $value;
-            echo "Foram transferidos B$" . $value . " da conta " . $from . " para a conta " . $to . ". O novo saldo da conta origem é de B$" . $balanceFrom . ". O novo saldo da conta destino é de B$" . $balanceTo . ".\n \n ";
+            echo "Foram transferidos B$" . number_format($value, 2, ',', '.') . " da conta " . $from . " para a conta " . $to . ". O novo saldo da conta origem é de B$" . $balanceFrom . ". O novo saldo da conta destino é de B$" . number_format($balanceTo, 2, ',', '.') . ".\n \n ";
         }
     }
 }
