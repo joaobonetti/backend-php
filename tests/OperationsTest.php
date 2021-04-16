aa0fc280e7991a2c0edae5e8c813ecbbc029dbac
João Bonetti <joaopaulodalmeida2@gmail.com>
2021-04-15 22:03:50 -0300

    creation testDepositValue

--- tests/OperationsTest.php
+++ tests/OperationsTest.php
@@ -1,8 +1,16 @@
 <?php
 
+namespace Moovin\Job\Backend;
+
 use \PHPUnit_Framework_TestCase as TestCase;
 
 class OperationsTest extends TestCase
 {
+    public function testDepositValue()
+    {
+        $operations = new Operations();
+ 
+        $this->assertEquals(800.50, $operations->deposit(500.00, 300.50));
+    }
 
 }
