c3cab1b10291338af0479def6fabb6914ad68f0d
João Bonetti <joaopaulodalmeida2@gmail.com>
2021-04-15 22:46:51 -0300

    OperationsTest finished

--- tests/OperationsTest.php
+++ tests/OperationsTest.php
@@ -4,50 +4,49 @@ namespace Moovin\Job\Backend;
 
 require_once('config/Config.php');
 
-
 use \PHPUnit_Framework_TestCase as TestCase;
 
 class OperationsTest extends TestCase
 {
-    // public function testDepositValue()
-    // {
-    //     $operations = new Operations();
+    public function testDepositValue()
+    {
+        $operations = new Operations();
  
-    //     $this->assertSame(800.50, $operations->deposit(500.00, 300.50));
-    // }
+        $this->assertSame(800.50, $operations->deposit(500.00, 300.50));
+    }
 
-    // public function testWithdrawTrue()
-    // {
-    //     $operations = new Operations();
+    public function testWithdrawTrue()
+    {
+        $operations = new Operations();
  
-    //     $this->assertSame(497.50, $operations->withdraw(1000, 500, 'CC'));
-    // }
+        $this->assertSame(497.50, $operations->withdraw(1000, 500, 'CC'));
+    }
 
-    // public function testWithdrawWithoutBalance()
-    // {
-    //     $operations = new Operations();
+    public function testWithdrawWithoutBalance()
+    {
+        $operations = new Operations();
  
-    //     $this->assertSame(100, $operations->withdraw(100, 500, 'CC'));
-    // }
+        $this->assertSame(100, $operations->withdraw(100, 500, 'CC'));
+    }
 
-    // public function testWithdrawOnLimit()
-    // {
-    //     $operations = new Operations();
+    public function testWithdrawOnLimit()
+    {
+        $operations = new Operations();
  
-    //     $this->assertSame(100, $operations->withdraw(100, 601, 'CC'));
-    // }
+        $this->assertSame(100, $operations->withdraw(100, 601, 'CC'));
+    }
 
-    // public function testTransferTrue()
-    // {
-    //     $operations = new Operations();
+    public function testTransferTrue()
+    {
+        $operations = new Operations();
 
-    //     $arrayExpected = [
-    //         0 => 5534.0,
-    //         1 => 24565.27
-    //     ];
+        $arrayExpected = [
+            0 => 5534.0,
+            1 => 24565.27
+        ];
  
-    //     $this->assertSame($arrayExpected, $operations->transfer(123456, 5584.58, 987654, 50.58));
-    // }
+        $this->assertSame($arrayExpected, $operations->transfer(123456, 5584.58, 987654, 50.58));
+    }
 
     public function testTransferSameAccount()
     {
@@ -62,7 +61,4 @@ class OperationsTest extends TestCase
 
         $this->assertSame(False, $operations->transfer(348974, 7.87, 963852, 500.58));
     }
-
-
-
 }
